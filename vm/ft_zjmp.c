d746f76d79266a77048a8f233f365f8b66aeeeed
timorinn <timorinn@yandex.ru>
2019-12-17 10:45:51 +0300

    fixed zjmp t_dir -> t_ind

--- vm/ft_zjmp.c
+++ vm/ft_zjmp.c
@@ -3,29 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_zjmp.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
+/*   By: stasyan <stasyan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/14 22:39:44 by bford             #+#    #+#             */
-/*   Updated: 2019/12/14 22:56:38 by bford            ###   ########.fr       */
+/*   Updated: 2019/12/17 10:45:06 by stasyan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
+/*
+*	Испольую Т_ИНД, тк размер Т_ДИР для этой операции равен 2.
+*/
+
 int		ft_zjmp(unsigned char map[4096][4], careta *car)
 {
-	t_dir			dir;
+	t_ind		ind;
 
 	mvprintw(50, 200, "CARRy = %d", car[0].carry);
 	if (!car[0].carry)
 	{
-		car[0].position = car[0].position + 5 > 4095 ?
-		car[0].position + 5 - 4096 : car[0].position + 5;
+		car[0].position = car[0].position + 3 > 4095 ?
+		car[0].position + 3 - 4096 : car[0].position + 3;
 		return (1);
 	}
-	ft_init_t_dir(map, car[0].position + 1, &dir);
-	car[0].position = car[0].position + dir.data % IDX_MOD > 4095 ?
-	car[0].position + dir.data % IDX_MOD - 4096 :
-	car[0].position + dir.data % IDX_MOD;
+	ft_init_t_ind(map, car[0].position + 1, &ind);
+	car[0].position = car[0].position + ind.data % IDX_MOD > 4095 ?
+	car[0].position + ind.data % IDX_MOD - 4096 :
+	car[0].position + ind.data % IDX_MOD;
 	return (1);
 }
