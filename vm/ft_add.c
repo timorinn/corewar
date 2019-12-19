b9c0e64b0320f10bc7f9dcd995c69f7684108247
kpsylock <kpsylock@student.21-school.ru>
2019-12-19 19:43:49 +0300

    Workin' on add operation

--- vm/ft_add.c
+++ vm/ft_add.c
@@ -7,16 +7,18 @@
 int ft_add(unsigned char map[MEM_SIZE][4], t_cursor *car)
 {
 	unsigned int	args[4];
-	unsigned char	reg1;
-	unsigned char	reg2;
-	unsigned char	reg3;
+	unsigned int	reg1;
+	unsigned int	reg2;
+	unsigned int	reg3;
 
 	ft_init_args(map, car->position, args);
 	if (args[1] != REG_CODE || args[2] != REG_CODE || args[3] != REG_CODE)
 		return (-1);
-	reg1 = map[(car->position + 2) % MEM_SIZE][0] % 16;
-	reg2 = map[(car->position + 3) % MEM_SIZE][0] % 16;
-	reg3 = map[(car->position + 4) % MEM_SIZE][0] % 16;
+	reg1 = map[(car->position + 2) % MEM_SIZE][0];
+	reg2 = map[(car->position + 3) % MEM_SIZE][0];
+	reg3 = map[(car->position + 4) % MEM_SIZE][0];
+	if (reg1 >= REG_NUMBER || reg2 >= REG_NUMBER || reg3 >= REG_NUMBER)
+		return (-1);
 	car->registr[reg3] = car->registr[reg1] + car->registr[reg2];
 	car->carry = (car->registr[reg3] == 0 ? 1 : 0);
 	car->position += 4;
