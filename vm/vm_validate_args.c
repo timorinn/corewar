073992ac0a768ffa0283c13bc59cf316e94b9857
Brittanie Ford <bford@ox-q4.21-school.ru>
2019-12-23 21:01:15 +0300

    vm_validate_args

--- vm/vm_validate_args.c
+++ vm/vm_validate_args.c
@@ -1,16 +1,31 @@
-//
-// Created by Katharine Psylocke on 23/12/2019.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm_validate_args.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/23 20:28:11 by bford             #+#    #+#             */
+/*   Updated: 2019/12/23 20:59:55 by bford            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "vm.h"
 
-bool	vm_validate_args(t_args *args, char *validate)
+bool	vm_validate_args(t_args args, char *validate)
 {
 	int i;
 
 	i = 0;
-
+	while (i < MAX_ARGS_NUMBER - 1)
+	{
+		if ((args.types[i] == 0 && !ft_strncmp(&validate[i * 3], "---", 3)) ||
+		(args.types[i] == REG_CODE &&
+		(validate[i * 3] == '-' || args.nums[i] < 1 || args.nums[i] > 16)) ||
+		(args.types[i] == IND_CODE && validate[i * 3 + 1] == '-') ||
+		(args.types[i] == DIR_CODE && validate[i * 3 + 2] == '-'))
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
 }
-
-"101100111"
-"RID--DR-D"
