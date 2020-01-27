ef58624ecf371c374856ec5a28184b0f3d20f19d
Brittanie Ford <bford@oa-o2.21-school.ru>
2020-01-27 21:20:37 +0300

    added += 0xffff0000 for operation ld

--- vm_src/op_load.c
+++ vm_src/op_load.c
@@ -6,7 +6,7 @@
 /*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/28 14:02:28 by bford             #+#    #+#             */
-/*   Updated: 2020/01/25 14:12:11 by bford            ###   ########.fr       */
+/*   Updated: 2020/01/27 21:13:23 by bford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,16 @@ inline bool	op_load(uint8_t map[MEM_SIZE][4], bool is_idx_needed,
 	args.dir_size = 2;
 	if (is_idx_needed == true)
 		args.dir_size += 2;
+
 	vm_get_args(map, cur, &args);
 	if (vm_validate_args(args, "-IDR-----", 2))
 	{
 		vm_unfold_all(map, cur, &args, is_idx_needed);
 		vm_print_log_args(&args, 2, cycle->log);
-		cur->registr[args.nums[1]] = args.nums_unfolded[0];
+		if (!is_idx_needed)
+			cur->registr[args.nums[1]] = args.nums_unfolded[0] + 0xffff0000;
+		else
+			cur->registr[args.nums[1]] = args.nums_unfolded[0];
 		cur->carry = (cur->registr[args.nums[1]] == 0 ? 1 : 0);
 	}
 	else if (cycle->log == true)
