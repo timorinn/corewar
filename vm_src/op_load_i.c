b3267ed46e6d11972a82b6a886e6938b6b4a05e0
Brittanie Ford <bford@at-l7.21-school.ru>
2020-01-29 15:32:36 +0300

    fixed bug with ll < 0

--- vm_src/op_load_i.c
+++ vm_src/op_load_i.c
@@ -6,7 +6,7 @@
 /*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/28 14:02:32 by bford             #+#    #+#             */
-/*   Updated: 2020/01/25 14:09:10 by bford            ###   ########.fr       */
+/*   Updated: 2020/01/29 15:31:18 by bford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,8 @@ inline bool			op_load_i(uint8_t map[MEM_SIZE][4],
 		vm_unfold_all(map, cur, &args, true);
 		vm_print_log_args(&args, 3, cycle->log);
 		offset = (args.nums_unfolded[0] + args.nums_unfolded[1]);
-		offset %= (ll == false ? IDX_MOD : (offset + 1));
+		if (!ll)
+			offset %= IDX_MOD;
 		addr = (cur->position + offset) % MEM_SIZE;
 		addr += (addr < 0 ? MEM_SIZE : 0);
 		cur->registr[args.nums[2]] = vm_init_t_dir(map, addr, 4).data;
