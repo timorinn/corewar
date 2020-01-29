c54824a23969f4f599416f62d3ae87e3e76504cb
Brittanie Ford <bford@at-l7.21-school.ru>
2020-01-29 16:47:37 +0300

    fixed bug with invalid operation on start

--- vm_src/vm_make_start_list_cursor.c
+++ vm_src/vm_make_start_list_cursor.c
@@ -6,7 +6,7 @@
 /*   By: bford <bford@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 18:13:13 by bford             #+#    #+#             */
-/*   Updated: 2020/01/22 19:44:05 by bford            ###   ########.fr       */
+/*   Updated: 2020/01/29 15:31:44 by bford            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ inline static void		init_cursor(t_cursor *cursor, t_player *player,
 	cursor->carry = 0;
 	cursor->position = position;
 	cursor->operation = map[cursor->position][0];
-	cursor->cooldown = g_operation[cursor->operation - 1];
+	if (cursor->operation > 0 && cursor->operation < 17)
+		cursor->cooldown = g_operation[cursor->operation - 1];
+	else
+		cursor->cooldown = 1;
 	ft_bzero(cursor->registr, sizeof(int) * (REG_NUMBER + 1));
 	cursor->registr[1] = -player->num;
 	cursor->next = NULL;
